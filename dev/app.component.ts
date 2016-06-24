9d6d2d860109c5daa9ba7d162e7e10d77c87506f
Artem Zinoviev <arzinoviev@gmail.com>
2016-06-24 14:57:35 +0300

    custom sttribute directives

---
+++ dev/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+import {AttributeDirectivesComponent} from "./attribute-directives.component";
+
+@Component({
+    selector: 'my-app',
+    template: `
+      <my-attribute-directives></my-attribute-directives>
+    `,
+    directives: [AttributeDirectivesComponent]
+})
+export class AppComponent {
+
+}
