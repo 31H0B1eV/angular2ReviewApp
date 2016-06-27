4cae24d57bc5dcfa1cc961380d06585291dbb5f9
Artem Zinoviev <arzinoviev@gmail.com>
2016-06-27 11:24:01 +0300

    clear project files for shopping list project setup

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,16 +1,10 @@
 import {Component} from 'angular2/core';
-import {AttributeDirectivesComponent} from "./attribute-directives.component";
-import {StructuralDirectivesComponent} from "./structural-directives.component";
 
 @Component({
     selector: 'my-app',
     template: `
-      <my-attribute-directives></my-attribute-directives>
-      <br>
-      <h1>Structural Directives</h1>
-      <my-structural-directives></my-structural-directives>
-    `,
-    directives: [AttributeDirectivesComponent, StructuralDirectivesComponent]
+      
+    `
 })
 export class AppComponent {
 
